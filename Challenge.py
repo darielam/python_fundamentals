1ad0840b5a4ca2f5db78ce95a0bda9b642a0f8f7
Dariela Marissa Mendiola Lopez <dariela.mendiola@neoris.com>
2021-09-09 20:10:49 -0500

    iniciando cambios

---
+++ Challenge.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Crear la variable
+Challenge=("dariela",23,False,[2,4],1.58)
+
+
+# In[2]:
+
+
+#cambiar la tupla a lista
+Challenge2=list(Challenge)
+
+
+# In[3]:
+
+
+Challenge2
+
+
+# In[ ]:
+
+
+#poner los elementos de la lista en un diccionario
+DiccionarioC= {1: Challenge2[0], 2:Challenge2[1],3:Challenge2[2],4:Challenge2[3], }
+
